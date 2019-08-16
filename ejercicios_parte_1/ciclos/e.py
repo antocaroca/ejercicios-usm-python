af41810e0e94b142be32a2e9d1bc42a191130e88
antonella.caroca <antonella.caroca@cci-entel.cl>
2019-08-16 10:15:26 -0400

    numero e

--- ejercicios_parte_1/ciclos/e.py
+++ ejercicios_parte_1/ciclos/e.py
@@ -1,32 +1,35 @@
 """
-Desarrolle un programa que entregue un valor aproximado de e, calculando esta suma hasta que
- la diferencia entre dos sumandos consecutivos sea menor que 0,0001.
-Recuerde que el factorial n! es el producto de los números de 1 a n.
+Desarrolle un programa que entregue un valor aproximado de e, calculando esta suma hasta que la diferencia entre
+ dos sumandos consecutivos sea menor que 0,0001. Recuerde que el factorial n! es el producto de los números de 1 a n.
 """
-# fórmula si ya sé el factorial de n:
-# siguiente factorial ==> n! = n × (n-1)!
-
 # fórmula factorial de n
-n = int(input('Ingrese n: '))
+n = 1
 fact = 1
-for i in range(n, 0, -1): # lista inversa de números
-    fact*=i  # contador que multiplica la lista 
-print("factorial de n ingresado", fact)  # 1 dividido en resultado
 
-siguiente_fact = (n + 1) * fact
+while True:
 
-print("siguiente factorial", siguiente_fact)
+    for i in range(n, 0, -1): # lista inversa de números
+        fact*=i  # contador que multiplica la lista 
+    # print("factorial de n ingresado:", fact) 
+    # print("1/factorial:", 1/fact) 
 
-suma = (1/fact) + (1/siguiente_fact)
+# fórmula si ya sé el factorial de n:
+# siguiente factorial ==> n × (n-1)!
+    siguiente_fact = (n + 1) * fact
 
-print("suma: ", suma)
+    # print("siguiente factorial", siguiente_fact)
+    # print("1/siguiente_fact:", 1/siguiente_fact)
 
-resta = (1/fact) - (1/siguiente_fact)
+    resta =  (1/fact) - (1/siguiente_fact)
+    # print("resta:", resta)
 
-if resta < 0.0001:
-    print("resta:", resta)
-else:
-    print('es mayor')
+
+    if resta < 0.0001:
+        print("resta:", resta)
+        break
+    else:
+        n +=1
+        
 
 
 
