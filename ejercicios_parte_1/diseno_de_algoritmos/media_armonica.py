85c652aa7ef91525eda1c0b1faca10346fb914d7
antonella.caroca <antonella.caroca@cci-entel.cl>
2019-08-26 16:01:41 -0400

    media armonica

--- ejercicios_parte_1/diseno_de_algoritmos/media_armonica.py
+++ ejercicios_parte_1/diseno_de_algoritmos/media_armonica.py
@@ -2,26 +2,14 @@
 #   El programa primero debe preguntar al usuario cuántos números ingresará. A continuación, 
 # pedirá al usuario que ingrese cada uno de los n números reales. Finalmente, el programa mostrará el resultado.
 
-n = int(input('Cuántos números: '))
-i = 0
-count = 0
-lista = []
-
-while i < n:
-    i += 1
-    count+=1
-    c = int(input(f'n{count}: '))
-    lista.append(c)
-# print(lista)
-# print("n:",n)
+numeros = int(input('Cuántos números: '))
 
 contador = 0
-for i in lista:
-    contador += (1/i)
-print("H =", n/contador)
-    
-    
-    
-    
+x = 0
 
+while contador < numeros:
+    contador += 1
+    n =  int(input(f'n{contador} = '))
+    x =  x + (1/n)
 
+print(f'H = {numeros/x}')
